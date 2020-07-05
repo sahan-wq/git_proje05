d05dd17b4bda79fd73c5eaa739a0e9ca3da4b076
KASIM <Ibrahim>
2020-07-05 22:23:18 +0200

    new message

--- src/yenipackage/ClassYeni.java
+++ src/yenipackage/ClassYeni.java
@@ -3,8 +3,7 @@ package yenipackage;
 public class ClassYeni {
 
 	public static void main(String[] args) {
-		System.out.println("ilk commit masterdan");
-		System.out.println("Merhaba hocam");
+		
 		
 
 	}
